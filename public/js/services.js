9e9eef1890aaa7e894e9c24a352198c7dc273011
andersrex <arex@Anderss-MacBook-Pro.local>
2013-10-20 10:47:43 -0700

    remove crappy front end geocoding

--- public/js/services.js
+++ public/js/services.js
@@ -4,31 +4,4 @@
 
   services = angular.module('app.services', []);
 
-  services.factory('geocoder', function($q, $rootScope) {
-    var geocoder;
-
-    geocoder = new google.maps.Geocoder();
-    return {
-      geocode: function(address) {
-        var deferred;
-
-        deferred = $q.defer();
-        geocoder.geocode({
-          address: address
-        }, function(results, status) {
-          var location;
-
-          if (status === google.maps.GeocoderStatus.OK) {
-            location = results[0].geometry.location;
-          } else {
-            location = null;
-          }
-          deferred.resolve(location);
-          return $rootScope.$apply();
-        });
-        return deferred.promise;
-      }
-    };
-  });
-
 }).call(this);
